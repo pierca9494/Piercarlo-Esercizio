5adea50f4233bde4771de9b4cc5ddace92c350df
pierca9494 <pierca9494@gmail.com>
2024-11-06 10:45:01 +0100

    Teatro aggiornato 10:44 11-06
    Teatro aggiornato 10:44 11-06

--- 12-05-2024/Teatro.py
+++ 12-05-2024/Teatro.py
@@ -60,9 +60,13 @@ class PostoStandard(Posto):
 class Teatro:
     def __init__(self):
         self._posti = []
+        self.__postiVip = []
 
     def aggiungi_posto(self, posto: Posto):
-        self._posti.append(posto)
+        if isinstance(posto, PostoVIP):
+            self.__postiVip.append(posto)
+        else:
+            self._posti.append(posto)
 
     def prenota_posto(self, numero: int, fila: str):
         for posto in self._posti:
@@ -70,6 +74,13 @@ class Teatro:
                 posto.prenota()
                 return
         print(f"Posto {fila}{numero} non trovato.")
+        
+    def prenota_vip(self, numero:int, fila:str, servizi_extra:str):
+        for posto in self.__postiVip:
+            if posto.numero == numero and posto.fila == fila and posto.servizi_extra == servizi_extra:
+                posto.prenota()
+                return
+        print(f"Posto VIP {fila}{numero} non trovato.")
 
     def stampa_posti_occupati(self):
         posti_occupati = [f"{posto.fila}{posto.numero}" for posto in self._posti if posto.occupato]
@@ -77,6 +88,13 @@ class Teatro:
             print("Posti occupati:", ", ".join(posti_occupati))
         else:
             print("Nessun posto occupato.")
+            
+    def stampa_posti_vip(self):
+        posti_vip = [f"{posto.fila}{posto.numero}" for posto in self.__postiVip if posto.occupato]
+        if posti_vip:
+            print("Posti VIP occupati:", ", ".join(posti_vip))
+        else:
+            print("Nessun posto VIP occupato.")
 
 
 # Esempio di utilizzo:
@@ -85,6 +103,7 @@ teatro.aggiungi_posto(PostoVIP(1, 'A', 'Accesso al lounge'))
 teatro.aggiungi_posto(PostoStandard(2, 'B', 5.0))
 teatro.aggiungi_posto(Posto(3, 'C'))
 
-teatro.prenota_posto(1, 'A')
+teatro.prenota_vip(1, 'A', 'Accesso al lounge' )
 teatro.prenota_posto(2, 'B')
 teatro.stampa_posti_occupati()
+teatro.stampa_posti_vip()
